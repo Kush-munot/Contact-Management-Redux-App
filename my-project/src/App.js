c586472b378cedff3e2d47371bf2d0236e63af83
Kush Munot <munot.kgm@gmail.com>
2023-10-10 10:58:23 +0530

    :zap:

---
+++ my-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UserList from './features/Users/UserList';
+
+function App() {
+  return (
+    <div>
+      <UserList/>
+    </div>
+  );
+}
+
+export default App;
