78983525a79ef7f752ba2f6b822accc50fe8b40f
Kush Munot <munot.kgm@gmail.com>
2023-10-10 14:58:44 +0530

    :zap:

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -4,7 +4,8 @@ import UserList from './features/Users/UserList';
 
 function App() {
   return (
-    <div>
+    <div className='container mx-auto px-2 max-w-5xl pt-10 md:pt-32'>
+      <h1 className='text-center font-bold text-2xl text-gray-700'>CRUD Application using React and  Redux </h1>
       <UserList/>
     </div>
   );
---
+++ my-project/src/Components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({onClick, children}) => {
+  return (
+    <button>
+        
+    </button>
+  )
+}
+
+export default Button
